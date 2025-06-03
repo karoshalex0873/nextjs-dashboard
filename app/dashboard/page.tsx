c48757ea6baab3eb97a3fb507b5d7cda1d06f076
Alex Karobia <akarobia30@gmail.com>
2025-06-03 23:51:25 +0300

    fetching data

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,10 +1,24 @@
 import React from 'react'
+import { lusitana } from '../ui/fonts'
+import { fetchRevenue } from '../lib/data'
+import RevenueChart from '../ui/dashboard/revenue-chart'
 
-const page = () => {
+const page = async () => { 
+ const revenue= await fetchRevenue()
+
   return (
-    <div>
-      Dashboard Page
-    </div>
+    <main>
+      <h1 className={`${lusitana.className}mb-4 text-xl md:text-2xl`}>
+        Dashnoard
+      </h1>
+      <div className='grid gap-6 sm:grid-cols-2 lg:grid-cols-4'>
+
+      </div>
+      <div className='mt-6 grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8'>
+        <RevenueChart revenue={revenue} />
+
+      </div>
+    </main>
   )
 }
 
